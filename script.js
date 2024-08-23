a970f832a31fba99c91e4ec04ff47694734767c6
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2024-08-23 10:28:08 +0200

    first push
    basic onclick fuctions

---
+++ script.js
@@ -0,0 +1,24 @@
+let view = document.getElementById('app'); 
+let addColor;
+updateVeiw()
+
+function updateVeiw()
+{
+   view.innerHTML = 
+   `
+   <div class="clickDiv" onclick="setVariabelColor(this.innerHTML)">green</div>
+   <div class="clickDiv" onclick="setVariabelColor(this.innerHTML)">yellow</div>
+   <div class="clickDiv" onclick="setVariabelColor(this.innerHTML)">red</div>
+   <button onclick="color()">change color</button>
+   `;
+}
+
+function color() 
+{
+    document.body.style.backgroundColor = addColor;
+}
+
+function setVariabelColor(newColor) 
+{
+  addColor =  newColor;
+}
